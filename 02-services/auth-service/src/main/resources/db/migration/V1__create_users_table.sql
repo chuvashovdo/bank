0dcdb028059e971ce65fcae563775795e361c82d
Jack Noon <timeakk187@gmail.com>
2025-04-11 18:38:36 +0300

    Добавление миграций

---
+++ 02-services/auth-service/src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS users (
+    id VARCHAR(255) PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL
+); 
